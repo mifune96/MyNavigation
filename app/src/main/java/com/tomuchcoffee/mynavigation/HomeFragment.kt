dc7dfd8df10ab8007105b8d5ced271ab9587351b
mifune96 <imrank.sensea@gmail.com>
2021-12-13 11:09:52 +0700

    lern navigation componen

---
+++ app/src/main/java/com/tomuchcoffee/mynavigation/HomeFragment.kt
@@ -0,0 +1,36 @@
+package com.tomuchcoffee.mynavigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.findNavController
+import com.tomuchcoffee.mynavigation.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private var _binding : FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        view.findNavController().navigate(R.id.action_homeFragment_to_profileActivity)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+
+}
